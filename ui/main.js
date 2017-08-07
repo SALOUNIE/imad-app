c5a0aaee195f290882f25395328f34c7667c5d9e
Saloni <30631631+SALOUNIE@users.noreply.github.com>
2017-08-07 21:34:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById("main-text");
 element.innerHTML = "New-value";
+
+//move the image
+var img = document.getElementById("drogo");
+img.onclick = function()
+{
+    img.style.marginleft = "100px";
+}
